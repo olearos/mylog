2b5ca4733a2a6459d52d38c08f132900b7aa71d7
Ole Andreas Rosok <oleandreas@gmail.com>
2016-02-03 08:47:48 +0100

    added .gitignore

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'rb.php';
+
+R::setup("mysql:host=".getenv('MYSQL_HOST').";dbname=".getenv('MYSQL_DATABASE'), getenv('MYSQL_USER'), getenv('MYSQL_PASS'));
+R::setAutoResolve( TRUE );        //Recommended as of version 4.2
+
+$post = R::dispense('line');
+$post->text = date("Y-m-d H:i:s") . ": " . implode(" ", $_GET);
+
+
+
+if(isset($_GET['do']) && $_GET['do'] == "view") {
+    foreach(array_reverse(R::findAll("line"))  as $line) echo $line->id .  " " . $line->text . "<br>\n";
+} else {
+    $id = R::store($post);
+}
+
+
+
+
+
+
+
