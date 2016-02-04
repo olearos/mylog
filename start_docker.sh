f18b12b1f09aa5aa429d40d8cc69e4f0d9f9f00f
Ole Andreas Rosok <oleandreas@gmail.com>
2016-02-04 14:34:11 +0100

    :

--- start_docker.sh
+++ start_docker.sh
@@ -3,6 +3,6 @@
 docker build -t my-lamp-app .
 
 #docker run -d -p 80:12345 -p 3306:3306 my-lamp-app
-docker run -d -p 80:12345 -p 33061:3306 -e MYSQL_PASS="mypass" my-lamp-app
+docker run -d -p 80:80 -p 12345:12345 -p 33061:3306 -e MYSQL_PASS="mypass" my-lamp-app
 
 
