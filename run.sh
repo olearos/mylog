9652abfad5720ef442a26d7810159d6ce8cbdee6
Ole Andreas Rosok <oleandreas@gmail.com>
2016-02-04 14:22:37 +0100

    changes

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 export MYSQL_USER="root"
 export MYSQL_PASS="admin"
 export MYSQL_HOST="localhost"
-export MYSQL_DATABASE="loglines"
+export MYSQL_DATABASE="mylog"
 
 
 php --server localhost:12345
--- start_docker.sh
+++ start_docker.sh
@@ -2,6 +2,6 @@
 
 docker build -t my-lamp-app .
 
-docker run -d -p 80:80 -p 3306:3306 my-lamp-app
+docker run -d -p 80:12345 -p 3306:3306 my-lamp-app
 
 
